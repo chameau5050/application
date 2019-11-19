19f4c87124854b14cee652e1d115ff7255bef39c
fannybeaumier <beaumierfanny@yahoo.ca>
2019-11-19 14:32:01 -0500

    sdd desing

--- serialcom/Form1.Designer.cs
+++ serialcom/Form1.Designer.cs
@@ -33,7 +33,11 @@
             this.TXWINDOW = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.data = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.data)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // CONNECT
@@ -57,14 +61,14 @@
             // 
             // TXWINDOW
             // 
-            this.TXWINDOW.Location = new System.Drawing.Point(315, 46);
+            this.TXWINDOW.Location = new System.Drawing.Point(378, 368);
             this.TXWINDOW.Name = "TXWINDOW";
             this.TXWINDOW.Size = new System.Drawing.Size(100, 20);
             this.TXWINDOW.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(354, 137);
+            this.button1.Location = new System.Drawing.Point(240, 384);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -76,17 +80,52 @@
             // 
             this.data.AllowUserToAddRows = false;
             this.data.AllowUserToDeleteRows = false;
-            this.data.Location = new System.Drawing.Point(506, 46);
+            this.data.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.data.Location = new System.Drawing.Point(446, 182);
             this.data.Name = "data";
-            this.data.Size = new System.Drawing.Size(240, 150);
+            this.data.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.data.Size = new System.Drawing.Size(700, 114);
             this.data.TabIndex = 4;
             this.data.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.data_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1208, 101);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 56);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "BiblioRobot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(295, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "©";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1202, 521);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.data);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.TXWINDOW);
@@ -98,6 +137,8 @@
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.data)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,7 +150,10 @@
         private System.Windows.Forms.Button DECONNECTION;
         private System.Windows.Forms.TextBox TXWINDOW;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView data;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.DataGridView data;
     }
 }
 
